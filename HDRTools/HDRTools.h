03209dfab9cfe460b3db02ef5b4b021317e56be8
jpsdr <jpsdr.psx@free.fr>
2021-06-30 23:24:36 +0200

    Threadpool : Fix for too much cores.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- HDRTools/HDRTools.h
+++ HDRTools/HDRTools.h
@@ -23,7 +23,7 @@
 #include "./avisynth.h"
 #include "./ThreadPoolInterface.h"
 
-#define HDRTOOLS_VERSION "HDRTools 0.6.2 JPSDR"
+#define HDRTOOLS_VERSION "HDRTools 0.6.3 JPSDR"
 
 
 typedef struct _dataLookUp

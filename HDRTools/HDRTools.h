97a3019c4b6382990474929c8cccae3b29765991
jpsdr <jpsdr.psx@free.fr>
2025-02-08 12:13:06 +0100

    Update version.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- HDRTools/HDRTools.h
+++ HDRTools/HDRTools.h
@@ -23,7 +23,7 @@
 #include "./avisynth.h"
 #include "./ThreadPoolInterface.h"
 
-#define HDRTOOLS_VERSION "HDRTools 1.0.5 JPSDR"
+#define HDRTOOLS_VERSION "HDRTools 1.0.6 JPSDR"
 
 
 typedef struct _dataLookUp
